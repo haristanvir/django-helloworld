b34176ce9195e4f371c8f9715cc2588a0d490cae
haristanvir <45670522+haristanvir@users.noreply.github.com>
2022-04-21 02:15:18 -0400

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -1,1 +1,2 @@
-opentelemetry-instrument python manage.py runserver --noreload
+#opentelemetry-instrument python manage.py runserver --noreload
+OTEL_EXPORTER_OTLP_ENDPOINT=http://127.0.0.1:4317 OTEL_PROPAGATORS=xray OTEL_PYTHON_ID_GENERATOR=xray opentelemetry-instrument uwsgi --http :8000 --module helloworld.wsgi
