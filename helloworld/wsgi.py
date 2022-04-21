b8bb5a5ff0655b838d674fcc22173a9aa6149c63
haristanvir <45670522+haristanvir@users.noreply.github.com>
2022-04-21 12:54:14 -0400

    opentelemetry changes

--- helloworld/wsgi.py
+++ helloworld/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
 
 application = get_wsgi_application()
+application = OpenTelemetryMiddleware(application)
